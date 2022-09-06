ebd0ea58ac49c38633e2628d6ef79980a6b4c84f
Aydan <aydan2903@gmail.com>
2022-09-06 11:21:46 +0400

    palindrom number

---
+++ Palindrom/script.js
@@ -0,0 +1,26 @@
+const checkPalindrom = 'ana'
+//232;
+//'Sator Arepo tenet opera rotas'
+//'menem'
+//'ata'
+
+const palindrome = () => {
+  let word = checkPalindrom + "";
+  let first = 0;
+  let last = word.length - 1;
+
+  for (i = first; i < word.length; i++) {
+    if (first > last) break;
+    let firstChar = word[first].toLocaleLowerCase();
+    let lastChar = word[last].toLocaleLowerCase();
+
+    if (firstChar === lastChar) {
+      first += 1;
+      last -= 1;
+      console.log(`Palindrom word : ${word}`);
+    } else {
+      console.log(`This word not palindrom : ${word}`);
+    }
+  }
+};
+palindrome();
