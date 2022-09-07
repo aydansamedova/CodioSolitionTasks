f07d12c26a90b7c1b5c504db58a58620ac6d3836
Aydan <aydan2903@gmail.com>
2022-09-07 13:05:45 +0400

    started exchange app and added login

---
+++ Algorithms/KeyValue/script.js
@@ -0,0 +1,20 @@
+const arr =  [
+    { type:'A', value:'Tesla' },	
+    { type:'A', value:'GMC' },
+    { type:'B', value:'KIA'},
+    { type:'C', value:'Dodge' },
+    { type:'C', value:'Ford' }
+    ]
+    
+
+    const keyValue=()=>{
+        for(let i=0; i<arr.length; i++){
+        const {type, value}=arr[i];
+          // console.log(type, value)
+        //   if(type === arr[i+1].type){
+        //     console.log(value)
+        //   }
+        }
+    }
+
+    keyValue()
---
+++ exchangeapp/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import Login from './companents/Login/Login'
+function App() {
+  return (
+    <div className=''>
+<Login/>
+    </div>
+  );
+}
+
+export default App;
